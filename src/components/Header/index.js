db3210083dcff78007d4e1132c623b1d21420dbc
willroberttv <willtaavares@gmail.com>
2020-08-31 18:20:10 -0300

    feat:header adicionado

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "./styles";
+
+function Header() {
+  return (
+    <div>
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default Header;
